14d9fc5141c6fa97956c9de94f502d92bfa1fd69
sou963 <souravbosu844@gmail.com>
2026-05-13 22:03:26 +0600

    Update project

---
+++ src/app/class/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>This is class page.</h1>
+      <h3>Donot rewrite that?</h3>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function notFound() {
+  return (
+    <div>
+      <h3>This is not found page!</h3>
+    </div>
+  );
+}
+
+export default notFound;
